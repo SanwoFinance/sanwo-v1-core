12711f6bed4efaee1935fd8344e1177094c403d0
adeolu98 <hardeyholu98@gmail.com>
2021-05-16 10:25:14 +0100

    fix compiler errors

--- contracts/SanwoV1Router.sol
+++ contracts/SanwoV1Router.sol
@@ -94,7 +94,7 @@ contract DePayRouterV1 {
     for (uint i = 0; i < plugins.length; i++) {
       require(_isApproved(plugins[i]), 'DePay: Plugin not approved!');
       
-      ISanwoRouterV1Plugin plugin = ISanwoRouterV1Plugin(configuration.approvePlugin(plugins[i]));
+      ISanwoRouterV1Plugin plugin = ISanwoRouterV1Plugin(configuration.approvePlugins(plugins[i]));
 
       if(plugin.delegate()) {
         (bool success, bytes memory returnData) = address(plugin).delegatecall(abi.encodeWithSelector(
